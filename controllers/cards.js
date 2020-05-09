445a75c91ff3a05884b4d795a4f25b24a31ef439
Konstantin <1konstantinmikov@gmail.com>
2020-05-09 16:42:27 +0300

    Повторное удаление карточки теперь обрабатывается

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,17 +5,20 @@ const Card = require(path.join(__dirname, '../models/card'));
 function deleteCard(req, res) {
   Card.findById(req.params.id)
     .then((card) => {
+      if (!card) {
+        return Promise.reject(new Error("Карточка не найдена или уже удалена"));
+      }
       if (req.user._id !== card.owner.toString()) {
-        return Promise.reject(new Error(`Вы не можете удалять чужие карточки`));
+        return Promise.reject(new Error("Вы не можете удалять чужие карточки"));
       }
       return card._id;
     })
     .then((cardId) => {
       Card.findByIdAndRemove(cardId)
         .then((card) => {
-          res.send(`Вы только что удалили карточку ${card}`);
+          res.send({ "message": "Вы удалили карточку" });
         })
-        .catch((err) => res.status(500).send('Не удалось удалить карточку'));
+        .catch((err) => res.status(500).send({ "message": "Серверная ошибка: не удалось удалить карточку" }));
     })
     .catch((err) => {
       res.status(403).send({ "message": err.message });
@@ -26,13 +29,13 @@ function createCard(req, res) {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
-    .catch((err) => res.status(500).send('Не удалось создать карточку'));
+    .catch((err) => res.status(500).send({ "message": "Серверная ошибка: не удалось создать карточку" }));
 }
 
 function getCards(req, res) {
   Card.find({})
     .then((cards) => res.send(cards))
-    .catch((err) => res.status(500).send('Произошла неведомая ошибка'));
+    .catch((err) => res.status(500).send({ "message": "Серверная ошибка: что-то пошло не так" }));
 }
 
 module.exports = {
